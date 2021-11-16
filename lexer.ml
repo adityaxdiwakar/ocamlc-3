f7c39bdf89741df122c0c587a2404716e388bfe7
Aditya Diwakar <aditya@diwakar.io>
2021-11-16 14:49:41 -0500

    :art: Declare production type

--- lexer.ml
+++ lexer.ml
@@ -11,9 +11,17 @@ module Lexer =
             | Reg of string
             | Label of string
             | Ident of string
+        
+        type 't production = { 
+            regex: string,
+            ctor: 't -> token
+        }
 
-        let patterns = [
-            "[\\s,]+"
+        let productions = [
+            { regex = "[\\s,]+"; ctor = fun _ -> Ws };
+            { regex = ";.*"; ctor = fun x -> Comment x };
+            { regex = "0?x[0-9a-fA-F]+"; ctor = fun x -> Hex x };
+            { regex = "#?-?[0-9]+"; ctor = fun x -> Number x };
         ]
         
         let lex_line line = 
@@ -24,11 +32,19 @@ module Lexer =
                     match p with
                     | []     -> raise Not_found
                     | h :: t -> begin
-                        match Re.exec (Re.Pcre.regexp h) line ~pos:r with
+                        match Re.exec (Re.Pcre.regexp h.regex) line ~pos:r with
                         | exception Not_found -> begin
                             pattern_match t 
                         end
-                        | x -> x
+                        | x -> begin
+
+                        end
                     end
     end
+
+
+(*
+    Solution: we need to look through all productions
+    - when we fidn a match, we need to append to some type of list
+*)
             
