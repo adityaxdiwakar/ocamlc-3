dde0aa8825da9368889ead621638d16703e4f842
Aditya Diwakar <aditya@diwakar.io>
2021-11-15 22:30:09 -0500

    :zap: More progress on lexer

--- lexer.ml
+++ lexer.ml
@@ -1,7 +1,7 @@
 module Lexer =
     struct
         type tokens = 
-            | Ws of string
+            | Ws
             | Comment of string
             | Hex of string
             | Num of string
@@ -10,6 +10,25 @@ module Lexer =
             | Directive of string
             | Reg of string
             | Label of string
-            | Id of string
+            | Ident of string
+
+        let patterns = [
+            "[\\s,]+"
+        ]
+        
+        let lex_line line = 
+            let ret = [] in
+            let rec lx r = 
+                Printf.printf "%s %d" line r;
+                let rec pattern_match p =
+                    match p with
+                    | []     -> raise Not_found
+                    | h :: t -> begin
+                        match Re.exec (Re.Pcre.regexp h) line ~pos:r with
+                        | exception Not_found -> begin
+                            pattern_match t 
+                        end
+                        | x -> x
+                    end
     end
             
