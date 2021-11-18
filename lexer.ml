3dae1314d3c8a24a63c342c58caae8e7667f3b01
Aditya Diwakar <aditya@diwakar.io>
2021-11-17 21:04:50 -0500

    :sparkles: Finish individual line lexer

--- lexer.ml
+++ lexer.ml
@@ -12,7 +12,6 @@ module Lexer =
             | Directive of string
             | Reg of string
             | Label of string
-            | Ident of string
         
         type 't production = { 
             regex: string;
@@ -24,7 +23,12 @@ module Lexer =
             { regex = ";.*"; ctor = fun x -> Comment x };
             { regex = "0?x[0-9a-fA-F]+"; ctor = fun x -> Hex x };
             { regex = "#?-?[0-9]+"; ctor = fun x -> Num x };
-            { regex = "ADD|AND|BRn?z?p?|JMP|JSRR|JSR|LDI|LDR|LD|LEA|NOT|RET|RTI|STI|STR|ST|TRAP"; ctor = fun x -> Op x };
+            { regex = "ADD|AND|BRn?z?p?|JMP|JSRR|JSR|LDI|LDR|LD|LEA"
+                ^ "|NOT|RET|RTI|STI|STR|ST|TRAP"; ctor = fun x -> Op x };
+            { regex = "\\.ORIG|\\.END|\\.FILL|\\.BLKW|\\.STRINGZ"; 
+                ctor = fun x -> Directive x };
+            { regex = "[rR][0-6]"; ctor = fun x -> Reg x };
+            { regex = "[a-zA-Z0-9_-]+:"; ctor = fun x -> Label x };
         ]
 
         let token_print x = 
@@ -34,6 +38,9 @@ module Lexer =
             | Hex(v)        -> (Printf.sprintf "Hex(%s)" v)
             | Num(v)        -> (Printf.sprintf "Num(%s)" v)
             | Op(v)         -> (Printf.sprintf "Opcode(%s)" v)
+            | Directive(v)  -> (Printf.sprintf "Directive(%s)" v)
+            | Reg(v)        -> (Printf.sprintf "Register(%s)" v)
+            | Label(v)      -> (Printf.sprintf "Label(%s)" v)
             | _             -> (Printf.sprintf "Unrecognized()")
         
         let lex_line line = 
--- ocamlc3.ml
+++ ocamlc3.ml
@@ -9,5 +9,6 @@ open Lexer;;
                 (sprint_list_helper t) end
     in Printf.sprintf "[%s]" (sprint_list_helper lst);;
 
-let line = "  ,ADD x784  x7216 x8317  #-3921 #391 #019 ; text" in
+let line = ".ORIG ADEMERG: ,ADD R6, R5, R2 x784  x7216 x8317 "
+    ^ "#-3921 #391 #019 .END; text" in
 Printf.printf "%s\n" (sprint_list (Lexer.lex_line line));;
