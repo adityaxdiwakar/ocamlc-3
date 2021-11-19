d8978cd6d6c7254e5b5ef671274ac6f096e7691a
Aditya Diwakar <aditya@diwakar.io>
2021-11-18 23:26:01 -0500

    :lipstick: Clean ocamlc.ml and reindent w/ 8731bb54515a74be3885aa083daa635ec7d4ff50

--- ocamlc3.ml
+++ ocamlc3.ml
@@ -1,7 +1,5 @@
 open Lexer;;
 
 let lexed_file = Lexer.lex_file "input.asm" in
-let mapped_lexed_file : string list list = List.map begin
-    List.map Lexer.token_print end lexed_file in
-List.iter begin
-    List.iter(Printf.printf "%s\n") end mapped_lexed_file
+let mapped_lexed_file = List.map (List.map Lexer.token_print) lexed_file in
+List.iter (List.iter (Printf.printf "%s\n")) mapped_lexed_file
