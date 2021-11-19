d4ea22560101f443a924ce06b2d12d47d7d75b52
Aditya Diwakar <aditya@diwakar.io>
2021-11-18 23:09:06 -0500

    :lipstick: Carry over changes from 8731bb54515a74be3885aa083daa635ec7d4ff50

--- lexer.mli
+++ lexer.mli
@@ -1,19 +1,19 @@
-module Lexer :       
-    sig
-        type token =
-            Ws
-            | Comment of string
-            | Hex of string
-            | Num of string
-            | Str of string
-            | Op of string
-            | Directive of string
-            | Reg of string
-            | Label of string
-        type 't production = { regex : string; ctor : 't -> token; }
-        val productions : string production list
-        val token_print : token -> string
-        val lex_line : string -> token list
-        val sprint_list : token list -> string
-        val lex_file : string -> token list list
-    end
+module Lexer :     
+  sig
+    type token =
+      Ws
+      | Comment of string
+      | Hex of string
+      | Num of string
+      | Str of string
+      | Op of string
+      | Directive of string
+      | Reg of string
+      | Label of string
+    type 't production = { regex : string; ctor : 't -> token; }
+    val productions : string production list
+    val token_print : token -> string
+    val lex_line : string -> token list
+    val sprint_list : token list -> string
+    val lex_file : string -> token list list
+  end
