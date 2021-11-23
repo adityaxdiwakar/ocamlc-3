c0a11697c16adf98de7e387b489491b8d776dc82
Aditya Diwakar <aditya@diwakar.io>
2021-11-22 19:49:17 -0500

    :zap: Add more to parser.ml

--- parser.ml
+++ parser.ml
@@ -1,26 +1,21 @@
-(*
-  { regex = "ADD|AND|BRn?z?p?|JMP|JSRR|JSR|LDI|LDR|LD|LEA"
-    ^ "|NOT|RET|RTI|STI|STR|ST|TRAP"; ctor = fun x -> Op x };
-*)
-
 type opcode =
-  | ADD
-  | AND
-  | BR of int
-  | JMP
-  | JSRR
-  | JSR
-  | LDI
-  | LDR
-  | LD
-  | LEA
-  | NOT
-  | RET
-  | RTI
-  | STI
-  | STR
-  | ST
-  | TRAP
+  | Add
+  | And
+  | Br of int
+  | Jmp
+  | Jsrr
+  | Jsr
+  | Ldi
+  | Ldr
+  | Ld
+  | Lea
+  | Not
+  | Ret
+  | Rti
+  | Sti
+  | Str
+  | St
+  | Trap
 
 type directive = 
   | ORIG
@@ -35,3 +30,21 @@ type parsed_tokens =
   | Num         of int
   | Directive   of directive
   | Label       of string
+
+let opcode_str_to_type = function
+  | "ADD" -> Add
+  | "AND" -> And
+
+let token_imm_parse tokens = 
+  let parse_indv_token = function
+    | Ws | Comment    -> None
+    | Hex(v) | Num(v) -> begin
+      v |> fun x -> if String.get x 0 == 'x' then "0" ^ x else x end 
+    | Op(v)         -> begin
+      match v with 
+    end
+    | Directive(v)  -> (Printf.sprintf "Directive(%s)"  v)
+    | Reg(v)        -> (Printf.sprintf "Register(%s)"   v)
+    | Label(v)      -> (Printf.sprintf "Label(%s)"      v)
+    | _             -> (Printf.sprintf "Unrecognized()"  )
+
