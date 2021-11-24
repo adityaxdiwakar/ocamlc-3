826a2b804457f47167b9f9c0c53549aefcb3fb35
Aditya Diwakar <aditya@diwakar.io>
2021-11-23 19:08:41 -0500

    :zap: Add opcode translation types
    Also, added TODO for TRAP (as there can be inputs such as HALT:
    syntactic sugar for TRAP). Is there a better way to implement this?
    Perhaps using a productions list?

--- parser.ml
+++ parser.ml
@@ -32,17 +32,43 @@ type parsed_tokens =
   | Label       of string
 
 let opcode_str_to_type = function
-  | "ADD" -> Add
-  | "AND" -> And
+  | "ADD"   -> Add
+  | "AND"   -> And
+
+  (* match branch statements *)
+  | "BRp"   -> Br 1
+  | "BRz"   -> Br 2
+  | "BRzp"  -> Br 3
+  | "BRn"   -> Br 4
+  | "BRnp"  -> Br 5
+  | "BRnz"  -> Br 6
+  | "BRnzp" (* synonymous with "BR" *)
+  | "BR"    -> Br 7
+  (* finish matching branch statements *)
+
+  | "JMP"   -> Jmp
+  | "JSRR"  -> Jsrr
+  | "JSR"   -> Jsr
+  | "LDI"   -> Ldi
+  | "LDR"   -> Ldr
+  | "LD"    -> Ld
+  | "LEA"   -> Lea
+  | "NOT"   -> Not
+  | "RET"   -> Ret
+  | "RTI"   -> Rti
+  | "STI"   -> Sti
+  | "STR"   -> Str
+  | "ST"    -> St
+
+  (* TODO: extend to more traps *)
+  | "TRAP"  -> Trap
 
 let token_imm_parse tokens = 
   let parse_indv_token = function
     | Ws | Comment    -> None
-    | Hex(v) | Num(v) -> begin
-      v |> fun x -> if String.get x 0 == 'x' then "0" ^ x else x end 
-    | Op(v)         -> begin
-      match v with 
-    end
+    | Hex(v) | Num(v) ->
+      v |> fun x -> if String.get x 0 == 'x' then "0" ^ x else x 
+    | Op(v)         -> opcode_str_to_type v
     | Directive(v)  -> (Printf.sprintf "Directive(%s)"  v)
     | Reg(v)        -> (Printf.sprintf "Register(%s)"   v)
     | Label(v)      -> (Printf.sprintf "Label(%s)"      v)
